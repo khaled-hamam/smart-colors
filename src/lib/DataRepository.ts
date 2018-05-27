200237994b231bc3d1f70e28645bee22929dade7
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-05-27 17:01:43 +0200

    Fixing unfound directory bug

--- src/lib/DataRepository.ts
+++ src/lib/DataRepository.ts
@@ -1,16 +1,19 @@
 import fs from "fs";
 import path from "path";
+import mkdirp from "mkdirp";
 import { ITrainingData } from "./ITrainingData.interface";
 
 abstract class DataRepository {
     public static async save(data: ITrainingData[]) {
         JSON.stringify(data);
-        await fs.writeFile('./src/data/data.json', JSON.stringify(data), 'utf8', err => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Data file is saved successfully.');
-            } 
+        await mkdirp('./src/data', async (err) => {
+            await fs.writeFile('./src/data/data.json', JSON.stringify(data), { encoding: 'utf8', flag: 'w' }, err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Data file is saved successfully.');
+                } 
+            });
         });
     }
 
@@ -20,7 +23,12 @@ abstract class DataRepository {
             output: [0]
         }];
 
-        const data = fs.readFileSync('./src/data/data.json', 'utf8');
+        let data = ''; 
+        try {
+            data = fs.readFileSync('./src/data/data.json', 'utf8');
+        } catch (e) {
+            console.log('Error opening data file');   
+        }
         
         return JSON.parse(data || JSON.stringify(defaultData));
     }
