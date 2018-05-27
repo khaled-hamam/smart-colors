0fbcd923953e1e57404b3180a039fb28874421f9
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-05-27 07:02:16 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smart Colors
+
+A smart 3-color combinations generator using Typescript and Brain.js
---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+
+class App {
+    public async start() {
+        const app: express.Application = express();
+
+        // Setting View Template Engine
+        app.set('view engine', 'ejs');
+        // Serving Static Files
+        app.use(express.static('public'));
+        // Using Morgan for Logging
+        app.use(morgan('combined'));
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port: ${PORT}`);
+        });
+    }
+}
+
+const app = new App();
+app.start();
